d074d1aae91b9e2b5e8aca986fd267f5db9685ba
Erlis Brito <erlisbrito@gmail.com>
2021-11-03 17:56:47 -0300

    atualização sistema

---
+++ BRBSolution.NautVed/Controllers/ContactController.cs
@@ -0,0 +1,68 @@
+﻿using BRBSolution.NautVed.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BRBSolution.NautVed.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DispararEmail(DispararEmailViewModel dispararEmailViewModel)
+        {
+
+            SmtpClient client = new();
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("erlisbrito@gmail.com", "Veonc@270315");
+            MailMessage mail = new();
+            mail.Sender = new MailAddress("erlisbrito@gmail.com", "Erlis");
+            mail.From = new MailAddress("erlisbrito@gmail.com", "Erlis");
+            mail.To.Add(new MailAddress("erlisbrito@gmail.com", "Erlis"));
+            mail.Subject = "Contato";
+            mail.Body = $" Mensagem do site:<br/> Nome:{dispararEmailViewModel.Nome} <br/> Email :{dispararEmailViewModel.Email} <br/> Mensagem :{dispararEmailViewModel.Mensagem}";
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+            SmtpClient smtp = new();
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com"; //for gmail host  
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("FromMailAddress", "password");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Send(message);
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                // tratar excessão aqui 
+            }
+            finally
+            {
+                mail = null;
+            }
+            return View();
+        }
+
+
+        public bool ValidarEmail(string email)
+        {
+            Regex regex = new(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            return match.Success;
+        }
+
+    }
+}
---
+++ BRBSolution.NautVed/Models/DispararEmailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BRBSolution.NautVed.Models
+{
+    public class DispararEmailViewModel
+    {
+        
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Mensagem { get; set; }
+    }
+}
